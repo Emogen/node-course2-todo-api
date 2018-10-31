1b65a24526bdc8aeda2da0f3b22571066c9f62bf
Singgih Anggana <phoenixgame2@gmail.com>
2018-10-31 11:00:47 +0700

    Test the DLETE /todos/:id route

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -98,3 +98,69 @@ describe('GET /todos/:id',() => {
 		.end(done);
 	});
 });
+
+describe('DELETE /todos/:id',()=>{
+	it('should remove a todo',(done) => {
+		var hexId = todos[1]._id.toHexString();
+
+		request(app)
+		.delete(`/todos/${hexId}`)
+		.expect(200)
+		.expect((res) => {
+
+			expect(res.body.todo._id).toBe(hexId);
+		})
+		.end((err,res)=>{
+			if(err){
+				return done(err);
+			}
+			Todo.findById(hexId).then((todo)=>{
+				expect(todo).toNotExist();
+				done();
+			}).catch((e)=>{
+				return done(err);
+			})
+		});
+	});
+
+	it('should return 404 if todo not found',(done) => {
+		var hexIdRandom = new ObjectID().toHexString();
+		var hexId = todos[1]._id.toHexString();
+
+		request(app)
+		.delete(`/todos/${hexIdRandom}`)
+		.expect(404)
+		.end((err,res)=>{
+			if(err){
+				return done(err);
+			}
+			Todo.findById(hexId).then((todo)=>{
+				
+				expect(todo._id.toHexString()).toBe(hexId);
+				done();
+			}).catch((err)=>{
+				done(err);
+			});
+		});
+	});
+
+	it('should return 404 if object id is invalid',(done)=>{
+		var hexId = todos[1]._id.toHexString();
+
+		request(app)
+		.delete(`/todos/111111`)
+		.expect(404)
+		.end((err,res)=>{
+			if(err){
+				return done(err);
+			}
+			Todo.findById(hexId).then((todo)=>{
+				
+				expect(todo._id.toHexString()).toBe(hexId);
+				done();
+			}).catch((err)=>{
+			 	done(err);
+			});
+		});
+	});
+});
